b38317b641d883fc10d8c1feeff639979d7cb868
Dimaylo <09dima03@gmail.com>
2025-01-21 22:54:20 +0300

    v 0.1

--- main.go
+++ main.go
@@ -1,10 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
+
+var task string
+
+type requestBody struct {
+	Message string `json:"message"`
+}
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		fmt.Fprintf(w, "hello, %s!", task)
+	} else {
+		fmt.Fprintln(w, "not method")
+	}
+}
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	var reqBody requestBody
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+	task = reqBody.Message
+	fmt.Fprintln(w, "Task updated")
+}
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, World!")
@@ -13,5 +39,7 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/task", GetHandler).Methods("GET")
+	router.HandleFunc("/api/task", PostHandler).Methods("POST")
 	http.ListenAndServe(":8080", router)
 }
