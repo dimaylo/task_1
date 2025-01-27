7f6d8a37080b585bbe51fb3758a185f28c14d2c6
Dimaylo <09dima03@gmail.com>
2025-01-27 20:57:55 +0300

    v

--- main.go
+++ main.go
@@ -7,27 +7,18 @@ import (
 	"net/http"
 )
 
-package main
-
-import (
-"encoding/json"
-"fmt"
-"github.com/gorilla/mux"
-"net/http"
-)
-
 var task string
 
 type requestBody struct {
 	Message string `json:"message"`
 }
 
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
+
 func GetHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		fmt.Fprintf(w, "Task: %s", task)
-	} else {
-		fmt.Fprintln(w, "not method:")
-	}
+	fmt.Fprintf(w, "Task, %s!", task)
 }
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,24 +29,44 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	task = reqBody.Message
-	fmt.Fprintln(w, "Task updated:", task)
+	fmt.Fprintln(w, "Task, updated!", task)
 }
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		fmt.Fprintln(w, "Привет, Мир!")
-	} else {
-		fmt.Fprintln(w, "Неправильный метод")
+func CreateMessage(w http.ResponseWriter, r *http.Request) {
+	var msg Message
+	err := json.NewDecoder(r.Body).Decode(&msg)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
 	}
+	result := DB.Create(&msg)
+	if result.Error != nil {
+		http.Error(w, "Failed to create message", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(msg)
+}
 
+func GetMessage(w http.ResponseWriter, r *http.Request) {
+	var messages []Message
+	result := DB.Find(&messages)
+	if result.Error != nil {
+		http.Error(w, "Failed go get message", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(messages)
 }
 
 func main() {
-
+	InitDB()
+	DB.AutoMigrate(&Message{})
 	router := mux.NewRouter()
+	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
 	router.HandleFunc("/api/task", GetHandler).Methods("GET")
 	router.HandleFunc("/api/task", PostHandler).Methods("POST")
-	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/messages", CreateMessage).Methods("POST")
+	router.HandleFunc("/api/messages", GetMessage).Methods("GET")
 	http.ListenAndServe(":8080", router)
-
 }
